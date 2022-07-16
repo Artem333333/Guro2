3488e8729812080884cd777c73a6d7c3404e0e94
Artem333333 <82451512+Artem333333@users.noreply.github.com>
2022-07-16 11:33:37 +0300

    Add files via upload

---
+++ Guro 2/src/module-info.java
@@ -0,0 +1,22 @@
+module Artem {
+/*  
+Продемонстрировать применение переменной.
+Присвоить исходному файлу имя Exemple2.java.
+*/
+class Exemple2 {
+	public static void main(String args []) {
+		int var1; // Объявления пременной.
+		int var2; // Объявление еще одной пременной.
+		var1 = 1024; // Присваивание значения 1024 переменной var1.
+		System.out.println("var1 contains "+ var1);
+		var2 = var1 / 2;
+		System.out.print("var2 contains var1 / 2: ");
+		System.out.println(var2);
+		
+		
+		
+	}
+
+
+
+}
